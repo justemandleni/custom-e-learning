4e478c9ce9168090da846da07f718a85f92e89ad
Just Emandleni <s216673380@mandela.ac.za>
2020-08-18 04:02:42 +0200

    init

---
+++ app/src/main/java/com/s216673380/graad12lewetenskappe/MainActivity.java
@@ -0,0 +1,57 @@
+package com.s216673380.graad12lewetenskappe;
+
+import android.content.DialogInterface;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        mWebView.loadUrl("file:///android_asset/html/raw/contents.html");
+        mWebView.getSettings().setSupportZoom(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (mWebView.canGoBack())
+                    mWebView.goBack();
+                else {
+                    AlertDialog.Builder aBuilder = new AlertDialog.Builder(MainActivity.this);
+                    aBuilder.setMessage("Are you sure you want to close the application?").setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                    AlertDialog alertDialog = aBuilder.create();
+                    alertDialog.show();
+                }
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
